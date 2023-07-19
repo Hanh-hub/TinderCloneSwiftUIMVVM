7e6810326ee990071527a77d2e1941543ed766b4
hanh vo <hahnvo422@gmail.com>
2023-07-18 22:36:24 -0500

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by Hanh Vo on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
