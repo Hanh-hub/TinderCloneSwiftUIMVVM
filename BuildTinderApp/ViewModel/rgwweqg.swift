1c1306f5df40ab2b2790aa0bfc1769c3f8cebeaa
hanh vo <hahnvo422@gmail.com>
2023-07-25 12:07:03 -0500

    Remove unused file

---
+++ BuildTinderApp/ViewModel/rgwweqg.swift
@@ -0,0 +1,20 @@
+//
+//  rgwweqg.swift
+//  BuildTinderApp
+//
+//  Created by Hanh Vo on 7/25/23.
+//
+
+import SwiftUI
+
+struct rgwweqg: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct rgwweqg_Previews: PreviewProvider {
+    static var previews: some View {
+        rgwweqg()
+    }
+}
